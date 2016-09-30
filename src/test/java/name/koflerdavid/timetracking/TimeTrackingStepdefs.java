1c40a422d8f8c2809fb14528a54d8a481b760210
David Kofler <kofler.david@gmail.com>
2016-09-30 17:37:09 +0200

    Create timetracking feature and steps definitions

---
+++ src/test/java/name/koflerdavid/timetracking/TimeTrackingStepdefs.java
@@ -0,0 +1,120 @@
+package name.koflerdavid.timetracking;
+
+import cucumber.api.DataTable;
+import cucumber.api.java8.En;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.junit.Assert.*;
+
+
+public class TimeTrackingStepdefs implements En {
+    private final TaskProvider taskProvider;
+    private final TaskStore taskStore;
+    private final LogProvider logProvider;
+    private final LogStore logStore;
+    private RunningTask currentTask = null;
+
+
+    public TimeTrackingStepdefs() {
+        final InMemoryTaskManager taskManager = new InMemoryTaskManager();
+        this.taskProvider = taskManager;
+        this.taskStore = taskManager;
+
+        final InMemoryLogManager logManager = new InMemoryLogManager();
+        this.logProvider = logManager;
+        this.logStore = logManager;
+
+
+        Given("^there is a task \"([^\"]*)\"$", this::createOrGetExistingTask);
+
+        Given("^\"([^\"]*)\" is the current task since \"([^\"]*)\"$", this::startTaskAt);
+
+        When("^I stop the current task at \"([^\"]*)\"$", (final String endOfTaskDateTimeString) -> {
+            assertNotNull("There should be a running task", currentTask);
+
+            final Instant endOfTask = Instant.parse(endOfTaskDateTimeString);
+
+            logStore.stopTask(currentTask, endOfTask);
+
+            currentTask = null;
+        });
+
+        Then("^there should be the following log entry:$", (final DataTable dataTable) -> {
+            final Map<String, String> input = dataTable.asMap(String.class, String.class);
+
+            final String beginDateTimeString = input.get("begin");
+            final Instant beginning = Instant.parse(beginDateTimeString);
+
+            final String durationString = input.get("duration");
+            final Duration duration = Duration.parse(durationString);
+
+            final String taskName = input.get("task");
+
+            boolean found = false;
+            for (final TaskLog taskLog : logProvider.getLog()) {
+                // We will `continue` if a differing criterion was found.
+
+                if (!Objects.equals(taskName, taskLog.getTaskName())) {
+                    continue;
+                }
+
+                if (!Objects.equals(beginning, taskLog.getBeginning())) {
+                    continue;
+                }
+
+                if (!Objects.equals(duration, taskLog.getDuration())) {
+                    continue;
+                }
+
+                found = true;
+            }
+
+            assertTrue("The task log should have been present", found);
+        });
+
+        When("^I start \"([^\"]*)\" at \"([^\"]*)\"$", this::startTaskAt);
+
+        Given("^there is no task with name \"([^\"]*)\"$", (final String taskName) -> {
+            final Task task = taskProvider.getTaskByName(taskName);
+            assertNull("The task should not exist yet", task);
+        });
+
+        When("^I create a new task with name \"([^\"]*)\"$", (final String taskName) -> {
+            final Task newTask = taskStore.createTask(taskName);
+            assertNotNull("The task should have been created", newTask);
+            assertTaskNameIs(taskName, newTask);
+        });
+    }
+
+    private Task createOrGetExistingTask(final String taskName) {
+        Task task = taskProvider.getTaskByName(taskName);
+        if (null == task) {
+            task = taskStore.createTask(taskName);
+        }
+
+        assertNotNull("The task should have been created", task);
+        assertEquals("The task should have the name " + task, taskName, task.getName());
+
+        return task;
+    }
+
+    private void startTaskAt(final String taskName, final String beginningOfTaskDateString) {
+        final Instant beginning = Instant.parse(beginningOfTaskDateString);
+
+        if (null != currentTask) {
+            logStore.stopTask(currentTask, beginning);
+        }
+
+        currentTask = taskProvider.startTask(taskName, beginning);
+        assertTaskNameIs(taskName, currentTask.getTask());
+        assertEquals(beginning, currentTask.getBeginning());
+    }
+
+    private void assertTaskNameIs(final String taskName, final Task task) {
+        assertEquals("The task should have the specified name", taskName, task.getName());
+    }
+}
---
+++ src/test/resources/timetracking/timetracking.feature
@@ -0,0 +1,55 @@
+Feature: timetracking
+
+  Before one can start working on a task, it has to be created.
+  Creating a task is done by searching first.
+  If there are no results, one can proceed to create the task.
+  The task immediately starts and puts the old task in the backlog.
+
+  Scenario: Stopping a task
+    Given there is a task "Fix bug #21"
+    And "Fix bug #21" is the current task since "2016-09-20T10:00:00Z"
+    When I stop the current task at "2016-09-20T23:00:00Z"
+    Then there should be the following log entry:
+      | begin    | 2016-09-20T10:00:00Z |
+      | duration | PT13H                |
+      | task     | Fix bug #21          |
+
+  Scenario: Starting a task which is already present
+    Given there is a task "Add feature #50"
+    When I start "Add feature #50" at "2016-07-20T09:05:30Z"
+    And I stop the current task at "2016-07-20T17:05:30Z"
+    Then there should be the following log entry:
+      | begin    | 2016-07-20T09:05:30Z |
+      | duration | PT8H                 |
+      | task     | Add feature #50      |
+
+  Scenario: Starting a task which is not yet present
+    Given there is no task with name "Fix bug #43"
+    When I create a new task with name "Fix bug #43"
+    And I start "Fix bug #43" at "2016-08-21T09:05:30Z"
+    And I stop the current task at "2016-08-21T10:05:30Z"
+    Then there should be the following log entry:
+      | begin    | 2016-08-21T09:05:30Z |
+      | duration | PT1H                 |
+      | task     | Fix bug #43          |
+
+  Scenario: Interrupting a task with a new task
+    Given there is a task "Add feature #50"
+    And "Add feature #50" is the current task since "2016-08-20T09:40:30Z"
+    And there is no task with name "Fix bug #43"
+    When I create a new task with name "Fix bug #43"
+    And I start "Fix bug #43" at "2016-08-20T10:00:30Z"
+    Then there should be the following log entry:
+      | begin    | 2016-08-20T09:40:30Z |
+      | duration | PT20M                |
+      | task     | Add feature #50      |
+
+  Scenario: Interrupting a task with an existing task
+    Given there is a task "Add feature #50"
+    And there is a task "Fix bug #42"
+    And "Add feature #50" is the current task since "2016-08-09T10:15:00Z"
+    When I start "Fix bug #42" at "2016-08-09T11:15:00Z"
+    Then there should be the following log entry:
+      | begin    | 2016-08-09T10:15:00Z |
+      | duration | PT1H                 |
+      | task     | Add feature #50      |
