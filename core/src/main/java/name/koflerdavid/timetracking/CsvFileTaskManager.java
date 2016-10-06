2926f473fbe12413d6b3b0ce1f0da077e9f39ef4
David Kofler <kofler.david@gmail.com>
2016-10-06 19:45:05 +0200

    Write new tasks to the task file.

--- core/src/main/java/name/koflerdavid/timetracking/CsvFileTaskManager.java
+++ core/src/main/java/name/koflerdavid/timetracking/CsvFileTaskManager.java
@@ -9,8 +9,12 @@ import java.util.TreeMap;
 
 public class CsvFileTaskManager implements TaskManager {
     private final Map<String, Task> tasks;
+    private final String tasklistFile;
+
 
     public CsvFileTaskManager(final String tasklistFile) throws TimeTrackingException {
+        this.tasklistFile = tasklistFile;
+
         try (final FileReader reader = new FileReader(tasklistFile)) {
             tasks = parseTasks(new BufferedReader(reader));
         } catch (final IOException e) {
@@ -26,13 +30,33 @@ public class CsvFileTaskManager implements TaskManager {
         return tasks;
     }
 
+    public void writeTasksToFile() throws TimeTrackingException {
+        try (final BufferedWriter writer = new BufferedWriter(new FileWriter(tasklistFile))) {
+
+            for (final Task task : tasks.values()) {
+                writer.write(task.getName());
+                writer.newLine();
+            }
+
+        } catch (final IOException e) {
+            throw new TimeTrackingException("Could not save tasks", e);
+        }
+    }
+
     @Override
     public Task getTaskByName(final String taskName) {
         return tasks.get(taskName);
     }
 
     @Override
-    public Task createTask(final String taskName) {
-        return tasks.computeIfAbsent(taskName, DefaultTask::new);
+    public Task createTask(final String taskName) throws TimeTrackingException {
+        Task task = tasks.get(taskName);
+
+        if (null == task) {
+            task = createTask(taskName);
+            writeTasksToFile();
+        }
+
+        return task;
     }
 }
